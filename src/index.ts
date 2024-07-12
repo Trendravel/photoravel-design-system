835713d7c33b7b2e0d0caeea2752995fd792c17e
devjaeyoon <devjaeyoon@gmail.com>
2024-07-12 19:30:31 +0900

    feat: 루트 디렉토리에서 각종 모듈을 import 해오기 위한 index 생성

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import Button from './components/Button/Button';
+import Input from './components/Input/Input';
+import PhotoravelProvider from './PhotoravelProvider';
+import theme from './styles/theme';
+import convertCSS from './utils/convertCSS';
+
+export { Button, Input, PhotoravelProvider, theme, convertCSS };
